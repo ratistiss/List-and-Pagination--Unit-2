d9d61b9a9fc94e2b29f86c1919722e399e0615e5
ratistiss <50683985+ratistiss@users.noreply.github.com>
2019-06-26 08:51:42 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,29 +16,13 @@ FSJS project 2 - List Filter and Pagination
    scoped to that function.
 ***/
 
-let studentList = document.getElementsByClassName('student-list');
-//let page = document.querySelectorAll('.page');
+let studentList = document.querySelector('.student-list li');
+let items = 10;
 
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-   
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 
 const showPage = (studentList, page) => {
-   let startIndex = (page * 10) - 10;
-   let endIndex = page * 10;
+   let startIndex = (page * items) - items;
+   let endIndex = page * items;
    for (let x = 0; x < studentList.length; x += 1);
       if ((x >= startIndex) && (endIndex <= x)){
          studentList.style.display = "block";
@@ -46,46 +30,36 @@ const showPage = (studentList, page) => {
          studentList.style.display = "none";
       }
    }
-
-}
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 
 let appendPageLinks = () => {
-   const div = document.createElement("div");
+   const div = document.createElement('div');
    const ul = document.createElement('ul');
    div.className = 'pagination';
    document.querySelector(".page").appendChild(div);
    div.appendChild(ul);
-   let li = document.createElement('li');
-   let anchor = document.createElement('a');
-       let pages = Math.ceil(studentList.length / 10);
-         for (let x = 0; x < pages.length; x += 1){
-           li;
-           anchor;
+       let pages = Math.ceil(studentList.length / items);
+         for (let x = 0; x < pages; x += 1){
+            let li = document.createElement('li');
+            let anchor = document.createElement('a');
            anchor.href = "#" ;
            anchor.textContent = x;
             li.appendChild(anchor);
             ul.appendChild(li);
      }
      
-}
-
-
-anchor.addEventListener('click', (e) => {
-   const a = document.querySelector('a');
-   for (let i = 0; i < anchor.length; i += 1){
-      a.className.remove = 'active'
-   }
-event.target.className = 'active';
-showPage(studentList, anchor.textContent);
-
-
+     anchor.addEventListener('click', (e) => {
+      const a = document.querySelector('a');
+       for (let i = 0; i < anchor.length; i += 1){
+        a.className.remove = 'active'
+      }
+   event.target.className = 'active';
+   showPage(studentList, anchor.textContent);
 });
-
+}
 
 appendPageLinks(studentList);
 showPage(studentList, appendPageLinks());
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
