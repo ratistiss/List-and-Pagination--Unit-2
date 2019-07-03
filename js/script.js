9e1dd0a131b0f65d339460e8d16d8be2305b23d7
ratistiss <50683985+ratistiss@users.noreply.github.com>
2019-07-03 15:05:43 -0400

    additional notes
    added in additional notes

--- js/script.js
+++ js/script.js
@@ -3,49 +3,33 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-let studentList = document.querySelectorAll(".student-item");
-let items = 10;
+let studentList = document.querySelectorAll(".student-item");                          //Global variable for the list of students
+let items = 10;                                                                        // Global variable for the 10 items per page
 
 
-const showPage = (studentList, page) => {
-   let startIndex = (page * items) - items;
-   let endIndex = (page * items);
+const showPage = (studentList, page) => {                                              // showPage function controls which students are shown
+   let startIndex = (page * items) - items;                                            // startIndex is for which student appears at the top of the page
+   let endIndex = (page * items);                                                      // endIndex is for which student appears at the bottom of the page 
    for (let x = 0; x < studentList.length; x += 1){
       if ((x >= startIndex) && (endIndex > x)){
-         studentList[x].style.display = "block";
+         studentList[x].style.display = "block";                                       //displays which students are on the page
       } else {
-         studentList[x].style.display = "none";
+         studentList[x].style.display = "none";                                        //Hides which students shouldnt be on the page
       }
    }
 }
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+
+
 const div = document.createElement('div');
 const ul = document.createElement('ul');
 div.className = 'pagination';
 document.querySelector(".page").appendChild(div);
 div.appendChild(ul);
 
-const appendPageLinks = () => {
-   //
-   const page = Math.ceil(studentList.length / items);
-   //console.log(pages);
-   for (let x = 0; x < page; x += 1){
+const appendPageLinks = () => {                                                        //Page Links Function
+   const page = Math.ceil(studentList.length / items);                                 // equation to determine pages needed
+   for (let x = 0; x < page; x += 1){                                                  //Loop to create elements needed for page
       const li = document.createElement('li');
       const anchor = document.createElement('a');
       anchor.href = "#";
@@ -54,18 +38,18 @@ const appendPageLinks = () => {
       ul.appendChild(li);
    }
    
-   showPage(studentList, 1);
+   showPage(studentList, 1);                                                           //to display first page when html doc is loaded
   
 
    
-   ul.addEventListener('click', (e) => {
+   ul.addEventListener('click', (e) => {                                               //event listener for pagination links
       e.preventDefault();
-      const anchor = document.querySelectorAll("a");
+      const anchor = document.querySelectorAll("a");                                   
        for (let i = 0; i < page; i += 1){
-        anchor[i].classList.remove('active');
-      if (anchor[i].textContent === e.target.textContent) {
+        anchor[i].classList.remove('active');                                          //removes class 'active' when somewhere else is clicked
+      if (anchor[i].textContent === e.target.textContent) {                            // adds class 'active to new  link clicked
          e.target.className = "active";
-         showPage(studentList, e.target.textContent);
+         showPage(studentList, e.target.textContent);                                  //showPage function for when a link is clicked
      }}});
 
 }
