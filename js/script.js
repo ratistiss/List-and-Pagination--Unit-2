3c9e5cdfef418197d9e273384e3b59289893c3fb
ratistiss <50683985+ratistiss@users.noreply.github.com>
2019-07-03 15:44:21 -0400

    Finsihed Meets Expectations

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ FSJS project 2 - List Filter and Pagination
    
 
 let studentList = document.querySelectorAll(".student-item");                          //Global variable for the list of students
-let items = 10;                                                                        // Global variable for the 10 items per page
+let items = 10;                                                                        //Global variable for the 10 items per page
 
 
-const showPage = (studentList, page) => {                                              // showPage function controls which students are shown
-   let startIndex = (page * items) - items;                                            // startIndex is for which student appears at the top of the page
-   let endIndex = (page * items);                                                      // endIndex is for which student appears at the bottom of the page 
+const showPage = (studentList, page) => {                                              //showPage function controls which students are shown
+   let startIndex = (page * items) - items;                                            //startIndex is for which student appears at the top of the page
+   let endIndex = (page * items);                                                      //endIndex is for which student appears at the bottom of the page 
    for (let x = 0; x < studentList.length; x += 1){
       if ((x >= startIndex) && (endIndex > x)){
          studentList[x].style.display = "block";                                       //displays which students are on the page
@@ -21,30 +21,30 @@ const showPage = (studentList, page) => {                                       
 }
 
 
-const div = document.createElement('div');
-const ul = document.createElement('ul');
-div.className = 'pagination';
-document.querySelector(".page").appendChild(div);
-div.appendChild(ul);
+const div = document.createElement('div');                                             //div holding pagination links
+const ul = document.createElement('ul');                                               //unordered list element
+div.className = 'pagination';                                                          //added class name to div
+document.querySelector(".page").appendChild(div);                                      //appended div to a parent div
+div.appendChild(ul);                                                                   //appended ul to div
 
 const appendPageLinks = () => {                                                        //Page Links Function
    const page = Math.ceil(studentList.length / items);                                 // equation to determine pages needed
    for (let x = 0; x < page; x += 1){                                                  //Loop to create elements needed for page
-      const li = document.createElement('li');
-      const anchor = document.createElement('a');
-      anchor.href = "#";
-      anchor.textContent = x + 1;
-      li.appendChild(anchor);
-      ul.appendChild(li);
+      const li = document.createElement('li');                                         //creates each <li> element needed
+      const anchor = document.createElement('a');                                      //creates each <a> element needed
+      anchor.href = "#";                                                               //adds an href to each <a>
+      anchor.textContent = x + 1;                                                      //gives pagination links numbers(+1 so pagination links don't start at 0)
+      li.appendChild(anchor);                                                          //appends <li> to <a>
+      ul.appendChild(li);                                                              //appends <ul> to <li>
    }
    
-   showPage(studentList, 1);                                                           //to display first page when html doc is loaded
+   showPage(studentList, 1);                                                           //to display first page when html file is loaded
   
 
    
    ul.addEventListener('click', (e) => {                                               //event listener for pagination links
       e.preventDefault();
-      const anchor = document.querySelectorAll("a");                                   
+      const anchor = document.querySelectorAll("a");                                   //local variable to connect <a> to this function                                  
        for (let i = 0; i < page; i += 1){
         anchor[i].classList.remove('active');                                          //removes class 'active' when somewhere else is clicked
       if (anchor[i].textContent === e.target.textContent) {                            // adds class 'active to new  link clicked
@@ -53,5 +53,5 @@ const appendPageLinks = () => {                                                 
      }}});
 
 }
-appendPageLinks(studentList);
+appendPageLinks(studentList);                                                          //calls function to html file
 
